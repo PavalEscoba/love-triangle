71fbc36de99c9410bb58275022d740ea3559bb70
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-18 16:30:20 +0300

    Revert "second task solved"
    This reverts commit 6e4f85385d6cf8b03c439cb5cb2a205c2526694c.

--- src/index.js
+++ src/index.js
@@ -4,21 +4,4 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-
-
-    var result = 0;
-    for(var i=0; i<preferences.length; i++){
-
-
-      var firstBeloved = preferences[i] -1;
-
-      var secondBeloved = preferences[firstBeloved] - 1;
-
-      var thirdBeloved = preferences[secondBeloved] - 1;
-      if(thirdBeloved == i){
-        result +=1;
-      }
-
-    }
-    return Math.floor(result/3);
 };
